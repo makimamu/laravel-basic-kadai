a4a921f1b7e467e05b36d49bbd9719941ea34d06
makittya@gmail.com <makitty@gmail.com>
2024-10-24 02:09:29 +0900

    Initial commit for Laravel project

--- routes/web.php
+++ routes/web.php
@@ -36,10 +36,14 @@ require __DIR__.'/auth.php';
 //そのURLにアクセスしたときに実行する処理」を(ルーティング)に直接記述する代わりにpostControllerのindexアクションを指定。
 Route::get('/posts', [PostController::class, 'index']);
 // 投稿作成画面を表示するルート　(データの作成機能とバリデーションの実装)ーーーーーーーーーーーーー
-Route::get('/posts/create', [PostController::class, 'create']);
-// 投稿データを保存するルート
-Route::post('/posts', [PostController::class, 'store'])->name('posts.store');;
+Route::get('/posts/create', [PostController::class, 'create'])->middleware('auth');
 
+// 投稿データを保存するルート
+Route::post('/posts', [PostController::class, 'store'])->middleware('auth');
+
+Route::get('/posts/create', [PostController::class, 'create']);
+
+Route::post('/posts/store', [PostController::class, 'store']);
 /*【お願いリクエスト:「このページの情報をください」というお願いを出すGETメソッド】【名前がつけられたルートのことを、名前付きルートといいます。基本的に名前は自由につけられますが、posts.indexのように基準となるURL.アクション名とする。
 「/posts/1」「/posts/5」のように可変の値を含むURLにアクセスした際、PostControllerのshowアクションが実行されるようにルーティングを設定します。*/
 Route::get('/posts/{id}',[PostController::class, 'show']);
